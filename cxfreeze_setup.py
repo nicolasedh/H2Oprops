a8298bca0e396dea529344c77619abdb359666fe
Nicolas Edh <nicolas.edh@gmail.com>
2014-10-08 23:07:37 +0200

    Improved cx_freeze script

--- cxfreeze_setup.py
+++ cxfreeze_setup.py
@@ -34,6 +34,7 @@ if sys.platform == "win32":
     base = "Win32GUI"
 
 includes = [
+#            "H2Oprops_GUI",
 #            "atexit","re",
             "iapws",
 #            "numpy",
@@ -41,15 +42,30 @@ includes = [
             "scipy.sparse.csgraph._validation",
             "scipy.optimize.minpack", 
             "scipy.sparse.linalg.dsolve.umfpack"]
-
+includefiles = [
+#                ("iapws.py","src/iapws.py"),
+                ("LICENSE", "src/LICENSE"),
+                ("H2Oprops/H2Oprops.py","src/H2Oprops.py"),
+                ("H2Oprops/H2Oprops_GUI.py","src/H2Oprops_GUI.py"),
+                ("H2Oprops/H2Oprops.ui","src/H2Oprops.ui")
+            ]
 excludes = ["tk", "_tkagg", "_gtagg", "_gtk", "tcl"]
-exe = Executable("H2Oprops.py", base = base, compress = True)
+path=sys.path
+path.append("H2Oprops")
+exe = Executable("H2Oprops/H2Oprops.py", base = base,
+                 excludes=excludes,
+#                 path = path,
+#                    includes="H2Oprops_GUI", 
+                 compress = True,
+                 copyDependentFiles = True)
 
 setup(
         name = apptitle,
         version = "0.1",
         description = "Water and steam properties calculator",
         options = {"build_exe" : {"includes" : includes ,
+                                  "include_files" : includefiles,
+                                  "path" : path,
                                   "excludes" : excludes,
                                   "bin_excludes" : excludes,
                                   "include_msvcr":[]}},
